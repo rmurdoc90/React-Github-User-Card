4459af4e0ca92c388fc6f5a4abe7447ce805c8d4
Robert Murdoch <robertmurdoch@Roberts-iMac.local>
2020-05-05 19:15:49 -0700

    Everything completed aside from being able to axios call followers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Card from './components/Card';
+import "./App.css";
 
 
 class App extends React.Component {
@@ -8,25 +9,40 @@ class App extends React.Component {
   constructor (){
     super();
     this.state = {
+        myData:[],
         hubData:[]
     }
   }
 
   componentDidMount() {
     axios
-      .get("https://api.github.com/users/robertmurdoch/followers")
+      .get("https://api.github.com/users/rmurdoc90")
       .then(res => {
-        this.setState({hubData: res.data})
+        this.setState({myData: res.data})
         console.log(res.data)
       })
       .catch(err => console.log(err));
+
+    }
+
+    fetchData = e => {
+      e.preventDefault()
+        axios
+        .get("https://api.github.com/users/rmurdoc90/followers")
+        .then(res => {
+          this.setState({hubData: res.data})
+          console.log(res.data)
+        })
+        .catch(err => console.log(err));
   }
+
+
 
     render(){
       return (
         <div className="App">
           <h1>Welcome to the Thunderdome</h1>
-          <Card  hubData = {this.state.hubData}/>
+          <Card hubData = {this.state.hubData} myData = {this.state.myData}/>
         </div>
   );
  }
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -4,14 +4,19 @@ class Card extends React.Component {
 
     render(){
         return(
-            <div>
-                
+            <div className='section'>
+                <div className='card'>
+                    <h2>Name: {this.props.myData.login}</h2>
+                    <img src={this.props.myData.avatar_url}/>
+                    <h3>Github: {this.props.myData.html_url}</h3>
+                </div>
+
                 {this.props.hubData.map(hubData => (
                     
                     <div key = {hubData.id} className ='card'>
                         <img src={hubData.avatar_url}/>
                         <div>
-                           <h3>{hubData.login}</h3>
+                           <h3>Name: {hubData.login}</h3>
                            <h4>Github: {hubData.html_url}</h4> 
                         </div>
 
